d975e57241fd2170448bfcc4a3fb42591811527a
Abdallah Elagamy <abdallahelagamy2@gmail.com>
2024-04-16 20:55:55 +0200

    V1.0

---
+++ Week 1/week1/src/T3.java
@@ -0,0 +1,8 @@
+public class T3 {
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+            if((i&1)==0)
+                System.out.println(i);
+        }
+    }
+}
